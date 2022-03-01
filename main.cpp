5be687bc5777423791be70ee26eb5d36ea55cbdb
Tom de Geus <tom@geus.me>
2022-03-01 10:43:50 +0100

    Trying

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <xtensor/xtensor.hpp>
 #include <xtensor/xsort.hpp>
 
+template <class T>
+std::vector<size_t> myfunc(const T& scale)
+{
+    auto index = xt::unravel_index(xt::argmin(xt::abs(scale))(), scale.shape());
+    size_t e = index[0];
+    size_t q = index[1];
+    return std::vector<size_t>{e, q};
+}
+
 int main()
 {
-    xt::xtensor<double, 2> A = {{0, 1, 2}, {3, -1, 4}};
-    auto index = xt::unravel_index(xt::argmin(xt::abs(A))(), A.shape());
-    size_t r = index[0];
-    size_t c = index[1];
-    assert(r == 1);
-    assert(c == 1);
+    xt::xtensor<double, 2> A = {{0, 1, 2}, {3, 4, -1}};
+    auto index = myfunc(A);
+    assert(index[0] == 1);
+    assert(index[1] == 1);
 }
